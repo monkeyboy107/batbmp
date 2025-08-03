cd732363ef05b0a90ba0da987cc0c0e787002051
Isaac Kerley <isaackerley@gmail.com>
2025-08-03 13:34:21 -0600

    Creating more tests

--- test/routes.py
+++ test/routes.py
@@ -4,21 +4,24 @@ from utils import database
 
 class database_tests(unittest.TestCase):
   @classmethod
-  def setUp(cls):
+  def setUpClass(cls):
     cls.example_mac = 'ab:cd:ef:12:34:56'
     cls.db_path = 'test.db'
     cls.engine = database.generate_engine(f'sqlite:///{cls.db_path}')
     cls.db = database.db(cls.engine)
   
   @classmethod
-  def tearDown(cls):
+  def tearDownClass(cls):
     os.remove(cls.db_path)
     
+  def setUp(self):
+    pass
+
+  def retrieve_database_without_populating(self):
+    pass
+        
   def test_creating_and_retrieving_data(self):
-    self.db.add_host(self.example_mac)
-    
-    host = self.db.find_host(self.example_mac)['host']['mac']
-    self.assertEqual(host, self.example_mac)
+    pass
 
 if __name__ == '__main__':
   unittest.main()
