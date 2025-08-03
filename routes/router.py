77119dfdeb316cd18c925417582e6cd212a57697
Isaac Kerley <isaackerley@gmail.com>
2025-08-02 20:45:09 -0600

    Updating path

--- routes/router.py
+++ routes/router.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
+import yaml
 
 route = Blueprint('simple_page', __name__, template_folder='templates')
 
 @route.route('/register/<mac>')
 def show(mac):
-  return render_template('kickstart.conf.j2')
+  with open('config.yaml') as stream:
+    config = yaml.safe_load(stream)
+  return render_template('kickstart.conf.j2', **config)
