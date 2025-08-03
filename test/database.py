a33bafcbc2df634ef7b1e636cdb67071d60f96a8
Isaac Kerley <isaackerley@gmail.com>
2025-08-03 10:37:56 -0600

    Adding testing for database

--- test/database.py
+++ test/database.py
@@ -3,13 +3,15 @@ from utils import database
 
 class render_template(unittest.TestCase):
   def test_database(self):
-    engine = database.generate_engine()
+    engine = database.generate_engine('sqlite:///file.db')
     db = database.db(engine)
     
     example_mac = 'ab:cd:ef:12:34:56'
     db.add_host(example_mac)
     
-    print(db.find_host(example_mac))
+    host = db.find_host(example_mac).all()
+    self.assertTrue(len(host) == 1)
+    self.assertEqual(host[0].mac, example_mac)
 
 if __name__ == '__main__':
   unittest.main()
--- utils/database.py
+++ utils/database.py
@@ -1,13 +1,25 @@
 from typing import List, Optional
 from sqlalchemy import create_engine, select, ForeignKey, String, Column, Text
-from sqlalchemy.orm import Session, DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.orm import Session, DeclarativeBase, Mapped, mapped_column, relationship, registry
 from sqlalchemy_utils import database_exists, create_database
+
+# class Base(DeclarativeBase):
+#     pass
+
+mapper_registry = registry()
+Base = mapper_registry.generate_base()
+
+class Host(Base):
+    __tablename__ = "hosts"
+    mac: Mapped[str] = mapped_column(String(18), primary_key=True)
 
 class db:
   def __init__(self, engine):
     self.engine = engine
-    if not database_exsists(engine.url):
+    if not database_exists(engine.url):
       create_database(engine.url)
+      mapper_registry.metadata.create_all(engine)
+      Base.metadata.create_all(engine)
   
   def add_host(self, mac):
     host = Host(mac=mac)
@@ -16,18 +28,10 @@ class db:
       session.commit()
   
   def find_host(self, mac):
-    with Session(self.engine) as session:
-      stmt = select(base.host.where(host.mac.in_([mac])))
-      for host in session.scalers(stmt):
-        print(host)
-
-class Base(DeclarativeBase):
-    pass
-
-class Host(Base):
-    __tablename__ = "hosts"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    mac: Mapped[str] = mapped_column(String(18))
+    session = Session(self.engine)
+    stmt = select(Host).where(Host.mac == mac)
+    host = session.scalars(stmt)
+    return host
 
 def generate_engine(connection_uri='sqlite:///:memory:'):
   return create_engine(connection_uri, echo=True)
