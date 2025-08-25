3df75eec80cec4a72d21fecbff00898844b31cdf
Isaac Kerley <isaackerley@gmail.com>
2025-08-24 22:08:00 -0600

    This was meant to be a string

--- routers/autodiscovery.py
+++ routers/autodiscovery.py
@@ -7,7 +7,7 @@ router = APIRouter()
 
 @router.get('/autodiscovery/', tags=['autodiscovery'], response_class=HTMLResponse)
 async def discover_mac():
-  return templates.render_template('chain.ipxe.j2', scheme=dependencies.settings['server']['scheme'], batbmp_host=dependencies.settings['server']['host'], route=autodiscovery)
+  return templates.render_template('chain.ipxe.j2', scheme=dependencies.settings['server']['scheme'], batbmp_host=dependencies.settings['server']['host'], route='autodiscovery')
 
 @router.get('/autodiscovery/{mac}/', tags=['autodiscovery'], response_class=HTMLResponse)
 async def evaluate_destination(mac):
