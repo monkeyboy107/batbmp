14c651bf9d6a8a8196cd2bb1f97c75c765e0641a
Isaac Kerley <isaackerley@gmail.com>
2025-08-24 21:55:39 -0600

    Changing route

--- routers/autodiscovery.py
+++ routers/autodiscovery.py
@@ -5,6 +5,10 @@ from utils import templates
 
 router = APIRouter()
 
+@router.get('/autodiscovery/', tags=['autodiscovery'], response_class=HTMLResponse)
+async def discover_mac():
+  return template.render_template('chain.ipxe.j2', scheme=dependencies.settings['server']['scheme'], batbmp_host=dependencies.settings['server']['host'], route='/autodiscovery/{net0/mac}')
+
 @router.get('/autodiscovery/{mac}/', tags=['autodiscovery'], response_class=HTMLResponse)
 async def evaluate_destination(mac):
   host = dependencies.settings['database'].find_host(mac)
